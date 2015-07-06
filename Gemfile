1245924a70fd7699ae295c85424803ddbf8a4442
Rathius <rathius@icloud.com>
2015-07-06 11:34:09 -0400

    Add search functionality

--- Gemfile
+++ Gemfile
@@ -19,3 +19,4 @@ gem "fog-aws"
 gem "figaro"
 gem "mini_magick"
 gem 'devise'
+gem 'searchkick'
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   
   devise_for :users
   resources :restaurants do
+	  collection do
+		  get 'search'
+	  end
 	  resources :reviews, except: [:show, :index]
   end
   
+ 
   get 'pages/about'
 
   get 'pages/contact'
